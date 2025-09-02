40ff78d7b820ea70cdc1abca083e8b93970b109c
KenGi07 <ken.saavedra@ucas-edu.net>
2025-09-02 13:39:26 -0600

    variables parctice

--- notes/test.py
+++ notes/test.py
@@ -1,3 +1,13 @@
 # KS 6th Github connection
 
 print("Hi")
+
+color=input("what color is the sky?\n")
+
+animal=input("what animal can photosynthesis?\n")
+
+print("whats your favorite fish")
+
+answer=input()
+
+print("your animal is", animal, )
---
+++ practice/variables_practice.py
@@ -0,0 +1,23 @@
+# KS 6th Variables practice
+
+name=input("what is your name?\n")
+
+oneten=input("number 1-10\n")
+
+hundthou=input("number 100-1000\n")
+
+breakfast=input("what did you have for breakfast?\n")
+
+color=input("favorite color?\n")
+
+school=input("what is your school that you go to?\n")
+
+year=input("what is the year?\n")
+
+eye=input("what is your eye color?\n")
+
+age=input("what is your age?\n")
+
+subject=input("what is your favorite subject?\n")
+
+print ("your name is",name, "your number 1-10 is",oneten, "your number 100-1000 is" ,hundthou, "you ate" ,breakfast, "your favorite color is" ,color, "the school you go to is" ,school, "the year is" ,year, "your eyecolor is" ,eye, "your age is" ,age, "your favorite subject is" ,subject,)

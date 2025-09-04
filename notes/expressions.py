9cba949207c8659b73a12a548276905aefdd0d88
KenGi07 <ken.saavedra@ucas-edu.net>
2025-09-04 13:43:31 -0600

    expressions notes

---
+++ notes/expressions.py
@@ -0,0 +1,23 @@
+# KS 6th expressions notes
+
+# What is an algorithm?
+    # is a set of intrusction that accomplishes a task
+name=input("what is your name")
+print("Hello", name)
+
+length = 5
+width = 20
+area = length * width
+print(length, "x", width, "=", area)
+
+age1=14
+age2=14
+age3=15
+age4=15
+
+average = (age1 + age2 + age3+ age4)/4
+
+print(age1, "x", age2, "x", age3, "x", age4, "/4" "=")
+
+print("average as is" ,average)
+

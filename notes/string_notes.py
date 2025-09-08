94c1e177c3f2aa934b900e2273935ee083529e41
KenGi07 <ken.saavedra@ucas-edu.net>
2025-09-08 13:44:43 -0600

    string notes

--- notes/string_notes.py
+++ notes/string_notes.py
@@ -1,1 +1,13 @@
-print("i did it!")
+# KS 6th string notes
+
+first_name=input("what is your first name:\n").strip().title()
+
+last_name=input("what is your last name:\n").strip().title()
+
+full_name=first_name + " " + last_name
+
+sentence="the quick brown fox jumps over the lazy dog"
+
+print("welcome to my program", full_name + "!")
+
+

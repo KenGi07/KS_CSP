b99484ae090ba29a01c1a2aad140c9bf20d64176
KenGi07 <ken.saavedra@ucas-edu.net>
2025-09-10 13:31:56 -0600

    silly sentences

---
+++ practice/silly_sentences.py
@@ -0,0 +1,9 @@
+# KS 6th silly senteces
+
+verb=input("Tell me a verb?\n")
+
+adjective=input("Tell me an adjective?\n")
+
+name=input("Give me a relatives name\n")
+
+print("When i was walking around i saw",name+".", "I became excillirated because he was my best friend and he was" ,verb, "so i walked away. I saw a" ,adjective, "lamp so i picked it up and it exploded")

4e4a6b77f2ae4ce0a788ad8fcfcf00a0e5242149
KenGi07 <ken.saavedra@ucas-edu.net>
2025-09-22 13:41:20 -0600

    for loop notes

--- notes/for_loops.py
+++ notes/for_loops.py
@@ -12,3 +12,60 @@ print(f"the hour is: {hour}")
 
 # lists
 siblings=["kenji", "saori", "eiki"]
+print(siblings[1])
+print(siblings)
+siblings[2]= "saori"
+siblings[1]= "eiki"
+siblings.remove("kenji")
+print(siblings)
+# What is a loop? 
+    # a continous code that repeats
+# What are the two types of loops?
+    # for loop, while loop
+# What is iteration
+    # keeps track of loop number
+# What are lists? 
+    # multiple items in a variable
+# How do you make lists in python? 
+    # you do variable=["kenji", "saori", "eiki"]
+# How do you make for loops in python? 
+    
+for sibling in siblings:
+    #print(f"Hello {sibling}")
+    print("hi")
+
+for x in range(20,1,-1):
+    print(x)
+
+# How do you make while loops in python?
+
+#while True:
+    #print("Oh NO!")
+
+
+i = 1
+
+while i < 21:
+    if i%2==0:
+        print(f"{i} is even")
+    else:
+        print(f"{i} is odd")
+    i+= 1
+
+import random
+
+number= random.randint(1,20)
+x=1
+#while x != number:
+    #print("duck")
+    #x+=1
+
+#print("goose!")
+
+while True:
+    if number==x:
+        print("goose")
+        break
+    else:
+        print("duck")
+        x+=1

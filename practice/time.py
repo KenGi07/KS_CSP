2b1859854a13a38980aed805084986177c24131c
KenGi07 <ken.saavedra@ucas-edu.net>
2025-09-23 18:18:46 +0000

    time

--- practice/time.py
+++ practice/time.py
@@ -1,5 +1,18 @@
 # KS 6th time assignment 
+
 import datetime
 
-current=datetime.datetime.now()
-hour=current.hour
+
+current = datetime.datetime.now()
+time = current.hour
+
+if time >= 18:
+    print(f"Hello the time is {time}, good night")
+
+
+elif time > 12 and time < 18:
+    print(f"Hello The time is {time}, good afternoon")
+
+
+elif time >= 1 and time < 12:
+    print(f"Hello the time is {time}, good morning")

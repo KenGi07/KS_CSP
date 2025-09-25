1cfd5b314f10903292c63e4659e4049bec8c990a
KenGi07 <ken.saavedra@ucas-edu.net>
2025-09-25 19:17:54 +0000

    old enough

---
+++ practice/old_enough.py
@@ -0,0 +1,12 @@
+# KS 6th old enough
+
+age= int(input("what is your age\n"))
+
+if age>= 18:
+    print("you can vote")
+elif age>=16:
+    print("you can drive")
+elif age>=15:
+    print("you can get your permit")
+else:
+    print("you can go to school")

4216031dcf44c263931a103582aec44503d2e3b2
KenGi07 <ken.saavedra@ucas-edu.net>
2025-10-02 13:44:03 -0600

    c

---
+++ c_programming/notes/variables.c
@@ -0,0 +1,15 @@
+// KS 6th Variables
+
+#include <stdio.h>
+
+int main(void){
+    int num = 4;
+    float pi= 3.14;
+    char grade='A'; // char = one character
+    char name[]="Kenji";
+    
+    printf("%d", num);
+
+    return 0;
+}
+// we have to tell the computer what type of data there is static and num

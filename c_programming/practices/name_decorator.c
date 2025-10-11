75d9352129735f3738012ff3379b79be6520115b
KenGi07 <ken.saavedra@ucas-edu.net>
2025-10-11 05:36:21 +0000

    name decorator

--- c_programming/practices/name_decorator.c
+++ c_programming/practices/name_decorator.c
@@ -1,3 +1,18 @@
 // KS 6th name decorator
 #include <stdio.h>
 #include <string.h>
+
+int main(void){
+    char name[30];
+    char decoratedname[75];
+
+    printf("write your name down:");
+    scanf("%s", name);
+
+    strcat (decoratedname, ":3:3:3");
+    strcat (decoratedname, name);
+    strcat (decoratedname, ";3;3;3");
+    printf( "decorated name: %s", decoratedname);
+
+    return 0;
+}

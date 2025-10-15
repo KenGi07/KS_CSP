fe382263b306e9f831dbe3d800b00d64c9c87201
KenGi07 <ken.saavedra@ucas-edu.net>
2025-10-15 13:42:48 -0600

    assignments

---
+++ c_programming/practices/update_hello_word.c
@@ -0,0 +1,24 @@
+// KS 6th Updated hello world
+
+#include <stdio.h>
+
+void greet(const char name[]){
+    printf("hello %s\n\n", name);
+}
+
+int main() {
+    greet("mayzi");
+
+    greet("santi");
+
+    greet("ryan");
+
+    greet("natali");
+
+    greet("adelle");
+
+    return 0;
+}
+
+
+
